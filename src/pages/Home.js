9abec8ee04031f186f13c36bc0b44863746aa5c3
tarcea <tarcea@gmail.com>
2021-03-12 15:23:23 +0100

    combined inline styles with css file style for dynamic fetched data

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,12 +6,7 @@ import LazyLoad from "react-lazyload";
 import { HomeValue } from "../components/HomeValue/HomeValue";
 import { SectionHeader } from "../components/SectionHeader/SectionHeader";
 import { JoinCity } from "../components/JoinCity/JoinCity";
-import community from "../assets/Value_community.svg";
-import expert from "../assets/Value_expert.svg";
-import journey from "../assets/Value_journey.svg";
 import JoinCityData from "../Data/JoinCityData";
-import FeaturedArticlesData from "../Data/FeatueredArticlesData";
-import AuxServicesData from "../Data/AuxServicesData";
 import FeaturedArticle from "../components/FeaturedArticle/FeaturedArticle";
 import AuxService from "../components/AuxService/AuxService";
 import { OwnSection } from "../components/OwnSection/OwnSection";
@@ -21,18 +16,20 @@ import { JoinCommunity } from "../components/JoinCommunity/JoinCommunity";
 import { RequestNewCity } from "../components/RequestNewCity/RequestNewCity";
 import useFetchDoc from "../hooks/useFetchDoc";
 import useFetchArray from "../hooks/useFetchArray";
+import { GetWindowDimension } from "../utils/GetWindowDimension";
 
 export const Home = () => {
+  const { width } = GetWindowDimension();
   const [query, setQuery] = useState("");
   const [moreJoinCity, setMoreJoinCity] = useState(false);
   const [joinCity, setJoinCity] = useState(JoinCityData);
   // const { items, loading } = useFetch('landing_live');
-  const banners = useFetchArray('banners');
-  const texts = useFetchDoc('texts');
-  const places = useFetchDoc('places');
-  const features = useFetchDoc('features');
-  const cities = useFetchArray('cities');
-  const articles = useFetchArray('articles');
+  const banners = useFetchArray("banners");
+  const texts = useFetchDoc("texts");
+  const places = useFetchDoc("places");
+  const features = useFetchDoc("features");
+  const cities = useFetchArray("cities");
+  const articles = useFetchArray("articles");
 
   const isLoading = () => {
     return (texts.loading || places.loading || features.loading || cities.loading || articles.loading || banners.loading);
@@ -60,112 +57,11 @@ export const Home = () => {
           })
         );
   };
-  const one = `Globuzzer is a global network that provides the full relocating experience. 
-Find topics, join communities, attend events, book flights, and much more. `;
-  const two = `Reliable information shared by expats and locals. 
-Everything from visa requirements and local documentation to valuable tips.`;
-  const three = `We are locals. We are expats. 
-We are travelers. We are students. 
-Most importantly, we have been in the same spot, and we can support you. `;
-const staticPage = () => {
-  return (
-    <>
-      <LazyLoad>
-        <section className="section_header" id="section_header">
-          <p id="header_1">The global community of locals and expats</p>
-          <p id="header_2">Complete guidance when relocating to a new city</p>
-          <SearchCity />
-          <p id="header_suggestion">
-            Maybe{" "}
-            <a href="https://globuzzer.mn.co/groups/195831/feed">Stockholm</a>,
-            <a href="https://globuzzer.mn.co/groups/195832/feed"> Helsinki</a>{" "}
-            or <a href="https://globuzzer.mn.co/groups/195834/feed">Paris</a>?
-          </p>
-        </section>
-      </LazyLoad>
-      <section className="section_value">
-        <HomeValue
-          src={community}
-          imgCaption="All-in-one platform"
-          alt="value"
-          imgDescription={one}
-        />
-        <HomeValue
-          src={expert}
-          imgCaption="Trustworthy content"
-          alt="value"
-          imgDescription={two}
-        />
-        <HomeValue
-          src={journey}
-          imgCaption="Local communities"
-          alt="value"
-          imgDescription={three}
-        />
-      </section>
-      <section className="section_newcity" id="section_newcity">
-        {/* <p id="newcity_p">Move to a new city? </p> */}
-        <SectionHeader header="What is your next destination? " />
-        <div className="newcity_input">
-          <FiSearch className="search_icon" />
-          <input
-            type="text"
-            placeholder="Find your city"
-            id="newcity_input_city"
-            value={query}
-            onChange={(event) => setQuery(event.target.value)}
-          />
-        </div>
-        <div className="joincity_grid">
-          {joinCity.map((cityData, index) => (
-            <JoinCity cityData={cityData} key={index} />
-          ))}
-          {!moreJoinCity && joinCity.length > 0 && (
-            <JoinCity
-              cityData={{ name: "Explore more cities" }}
-              isViewMore
-              setMoreJoinCity={setMoreJoinCity}
-            />
-          )}
-          {moreJoinCity && joinCity.length > 0 && <RequestNewCity />}
-        </div>
-        <div className="no_item">
-          {joinCity.length === 0 && <RequestNewCity />}
-        </div>
-      </section>
-      <JoinCommunity />
-
-      <section className="featured_articles" id="featured_articles">
-        <SectionHeader header="Featured articles" />
-        {FeaturedArticlesData.map(({ title, ...otherProps }) => (
-          <FeaturedArticle key={title} title={title} {...otherProps} />
-        ))}
-        <div className="featured_articles_more">
-          <a
-            href="https://globuzzer.mn.co/feed?filters=articles"
-            className="featured_articles_more_anchor"
-          >
-            SEE ALL ARTICLES
-          </a>
-          <MdKeyboardArrowRight className="featured_articles_more_icon" />
-        </div>
-      </section>
-      <section className="aux_services" id="aux_services">
-        <SectionHeader header="Helpful services" />
-        {/* <div className="aux_list">
-          {AuxServicesData.map(({ name, ...otherProps }) => (
-            <AuxService name={name} key={name} {...otherProps} />
-          ))}
-        </div> */}
-      </section>
-    </>
-  );
-};
 
   return (
     <>
-      {isLoading() ? staticPage() : 
-      (
+      {isLoading() ? <div>Loading...</div>
+      : (
         <>
           <LazyLoad>
             <section
@@ -174,19 +70,44 @@ const staticPage = () => {
               style={{backgroundImage: `url(${banners.items[0].img})`}}
             >
               <p
-                id="header_1" 
-                style={texts.items[0].style}
+                id="header_1"
+                style={
+                  {...texts.items[0].style, fontSize: width > 1100 ? texts.items[0].style.fontSize : ""}
+                }
               >
                 {texts.items[0].content}
               </p>
-              <p id="header_2">{texts.items[1].content}</p>
+              <p
+                id="header_2"
+                style={
+                  {...texts.items[1].style, fontSize: width > 1100 ? texts.items[1].style.fontSize : ""}
+                }
+              >
+                {texts.items[1].content}
+              </p>
               <SearchCity />
               <p id="header_suggestion">
                 Maybe{" "}
-                <a href="https://globuzzer.mn.co/groups/195831/feed">{places.items[0].text}</a>,
-                <a href="https://globuzzer.mn.co/groups/195832/feed">{places.items[1].text}</a>
+                <a
+                  href={places.items[0].link}
+                  style={{color: places.items[0].color}}
+                >
+                  {places.items[0].text}
+                </a>,{" "}
+                <a
+                  href={places.items[1].link}
+                  style={{color: places.items[1].color}}
+                >
+                  {places.items[1].text}
+                </a>
                 {" "}
-                or <a href="https://globuzzer.mn.co/groups/195834/feed">{places.items[2].text}</a>?
+                or{" "}
+                <a
+                  href={places.items[2].link}
+                  style={{color: places.items[2].color}}
+                >
+                  {places.items[2].text}
+                </a>{" "}?
               </p>
             </section>
           </LazyLoad>
